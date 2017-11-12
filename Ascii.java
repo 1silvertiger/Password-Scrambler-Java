9f0a12e4b116d7fcd1f720d2329c055f96df35ea
silvertiger <1silvertiger@gmail.com>
2017-11-12 14:26:25 -0600

    Testing VCS integration in IntelliJ IDE (A comment was added)

--- Ascii.java
+++ Ascii.java
@@ -1,52 +1,53 @@
 
 public class Ascii {
-	
-	private int asciiCode;
-	private char character;
-	private boolean usable;
-	
-	public Ascii() {
-		asciiCode = 0;
-		character = '0';
-		usable = false;
-	}
-	
-	public Ascii(int asciiCode, char character) {
-		this.asciiCode = asciiCode;
-		this.character = character;
-		usable = false;
-	}
-	
-	public int getAsciiCode() {
-		return asciiCode;
-	}
-	
-	public void setAsciiCode(int asciiCode) {
-		this.asciiCode = asciiCode;
-	}
-	
-	public char getCharacter() {
-		return character;
-	}
-	
-	public void setCharacter(char character) {
-		this.character = character;
-	}
-	
-	public boolean isUsable() {
-		return usable;
-	}
-	
-	public void setUsable(boolean usable) {
-		this.usable = usable;
-	}
-	
-	public String toString() {
-		String temp1 = " ";
+
+    private int asciiCode;
+    private char character;
+    private boolean usable;
+
+    public Ascii() {
+        asciiCode = 0;
+        character = '0';
+        usable = false;
+    }
+
+    public Ascii(int asciiCode, char character) {
+        this.asciiCode = asciiCode;
+        this.character = character;
+        usable = false;
+    }
+    //adding a test change from InteliJ IDE to test VCS
+
+    public int getAsciiCode() {
+        return asciiCode;
+    }
+
+    public void setAsciiCode(int asciiCode) {
+        this.asciiCode = asciiCode;
+    }
+
+    public char getCharacter() {
+        return character;
+    }
+
+    public void setCharacter(char character) {
+        this.character = character;
+    }
+
+    public boolean isUsable() {
+        return usable;
+    }
+
+    public void setUsable(boolean usable) {
+        this.usable = usable;
+    }
+
+    public String toString() {
+        String temp1 = " ";
 //		System.out.println(getAsciiCode());
-		System.out.print(getCharacter() + " ");
-		System.out.println(isUsable());
-		return temp1;
-	}
-	
+        System.out.print(getCharacter() + " ");
+        System.out.println(isUsable());
+        return temp1;
+    }
+
 }
